c8ef64ca257ed81be4ef95698d0d8dfdf105dad3
coscorrodrift <juan.dyamamoto@gmail.com>
2022-12-29 01:27:59 +0100

    message

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,13 @@
 module.exports = function(eleventyConfig) {
+    
+    eleventyConfig.addPassthroughCopy('./src/css')
+    eleventyConfig.addPassthroughCopy('./src/images')
+    eleventyConfig.addPassthroughCopy('./src/js')
+    eleventyConfig.addPassthroughCopy('./src/plugins')
+    eleventyConfig.addPassthroughCopy('./src/favicon.ico')
     return {
         dir: {
-            input: "views",
+            input: "src",
             output: "public"
         }
     }
